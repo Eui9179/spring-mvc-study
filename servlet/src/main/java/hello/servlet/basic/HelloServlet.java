5b2be069d13d9d237ef864f217423c6717095c04
Eui9179 <outdark97@gmail.com>
2023-01-04 02:50:26 +0900

    servlet-basic

---
+++ servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,34 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/*
+* 단축키
+* ctrl + alt + v: 변수 자동 생성
+* ctrl + o: 오버라이드 함수 생성
+* */
+
+@WebServlet(name="helloServlet", urlPatterns="/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
+
+    }
+}
